67188a881e9a3455093c9e9f88b1438a4a63390b
kkirby04 <kierankirby123@gmail.com>
2023-05-12 08:47:28 +0100

    added comments

---
+++ printFunction/main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    # Read an integer from STDIN
+    n = int(input())
+
+    # Create a list comprehension to generate the numbers from 1 to n
+    numbers = [str(i+1) for i in range(n)]
+
+    # Join the numbers using an empty string as the separator and print on one line
+    print(''.join(numbers))
