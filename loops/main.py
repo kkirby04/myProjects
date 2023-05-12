7513eeece4b9e4a0fd5b2d675712af49b7bea609
kkirby04 <kierankirby123@gmail.com>
2023-05-12 08:39:45 +0100

    coments

---
+++ loops/main.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    # Read an integer from STDIN
+    n = int(input())
+    
+    # Iterate through the range of non-negative integers less than n
+    for i in range(n):
+        # Calculate the square of each number
+        square = i**2
+        
+        # Print the square on a separate line
+        print(square)
