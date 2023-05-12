c3ba3e6ec191b1101ec734975e28f72cfe532ad6
kkirby04 <kierankirby123@gmail.com>
2023-05-12 08:03:15 +0100

    adding comments
    adding comments

---
+++ simpleCalculator/main.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    #taking user input
+    a = int(input())
+    b = int(input())
+    
+    #calculating sum
+    sum = a + b
+    print(sum)
+    
+    #calculateing difference
+    difference = a - b
+    print(difference)
+    
+    #calculateing product
+    product = a * b
+    print(product)
+    
