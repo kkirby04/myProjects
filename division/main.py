2cc7200f6c7e304795a5ba554251fe253f843219
kkirby04 <kierankirby123@gmail.com>
2023-05-12 08:32:53 +0100

    .

---
+++ division/main.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    # Read the first integer from STDIN
+    a = int(input())
+    
+    # Read the second integer from STDIN
+    b = int(input())
+    
+    # Perform integer division using //
+    integer_division = a // b
+    # Print the result of integer division
+    print(integer_division)
+    
+    # Perform float division by converting integers to floats
+    float_division = float(a) / float(b)
+    # Print the result of float division
+    print(float_division)
